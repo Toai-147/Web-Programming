b0789c5d0f2ca5dd0938f257f571731fc4edd749
Toai-147 <toaihvcgcs230193@fpt.edu.vn>
2025-11-28 17:56:33 +0700

    Delete Week2 directory

--- Week2/form1.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$firstname = $_POST['firstname'];
-$lastname = $_POST["lastname"];
-echo 'welcome to our website, ' .
-htmlspecialchars($firstname, ENT_QUOTES,'UTF-8') . ' ' .
-htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!'; 
-?>
